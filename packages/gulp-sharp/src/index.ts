c2075c85723f0cb456eb00b6236e8857732c081d
DonMahallem <donmahallem@users.noreply.github.com>
2020-07-28 15:12:36 +0200

    chore: add gulp plugin implemenation

--- packages/gulp-sharp/src/index.ts
+++ packages/gulp-sharp/src/index.ts
@@ -1,3 +1,7 @@
 /*!
  * Source https://github.com/donmahallem/js-libs Package: gulp-sharp
  */
+
+export { ISharpConfig } from './handle-config';
+export { SharpHandler } from './handle-method';
+export { gulpSharp, IConfig } from './plugin';
