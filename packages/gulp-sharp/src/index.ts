f38de0513a48d9d2e9b1ad4eda782a9daee7447a
DonMahallem <donmahallem@users.noreply.github.com>
2020-08-03 11:24:03 +0200

    refactor(gulp-sharp): move IConfig to seperate source file

--- packages/gulp-sharp/src/index.ts
+++ packages/gulp-sharp/src/index.ts
@@ -2,6 +2,7 @@
  * Source https://github.com/donmahallem/js-libs Package: gulp-sharp
  */
 
+export { IConfig } from './config';
 export { ISharpConfig } from './handle-config';
 export { SharpHandler } from './handle-method';
-export { gulpSharp, IConfig } from './plugin';
+export { gulpSharp } from './plugin';
--- packages/gulp-sharp/src/plugin.ts
+++ packages/gulp-sharp/src/plugin.ts
@@ -3,18 +3,14 @@
  */
 
 import * as PluginError from 'plugin-error';
-import * as sharp from 'sharp';
 import { Transform } from 'stream';
 import * as through from 'through2';
 import * as VinylFile from 'vinyl';
-import { handleConfig, ISharpConfig } from './handle-config';
-import { handleMethod, SharpHandler } from './handle-method';
+import { IConfig } from './config';
+import { handleConfig } from './handle-config';
+import { handleMethod } from './handle-method';
 
 const PLUGIN_NAME: string = '__BUILD_PACKAGE_NAME__';
-export interface IConfig {
-    config?: sharp.SharpOptions;
-    transform: ISharpConfig | SharpHandler;
-}
 const handleTransformPromise = (prom: Promise<VinylFile.BufferFile>, callback: through.TransformCallback): void => {
     prom.then((convertedFile: VinylFile.BufferFile): void => {
         callback(undefined, convertedFile);
