760558cb6837718ebfa7f2a22aa1098de0e13004
DonMahallem <donmahallem@users.noreply.github.com>
2020-08-03 08:06:27 +0200

    save

--- packages/gulp-merge-coverage/src/index.ts
+++ packages/gulp-merge-coverage/src/index.ts
@@ -2,6 +2,4 @@
  * Source https://github.com/donmahallem/js-libs Package: gulp-sharp
  */
 
-export { ISharpConfig } from './handle-config';
-export { SharpHandler } from './handle-method';
-export { gulpSharp, IConfig } from './plugin';
+export { mergeCoverage as gulpSharp, IConfig } from './plugin';
--- packages/gulp-merge-coverage/src/plugin.ts
+++ packages/gulp-merge-coverage/src/plugin.ts
@@ -2,41 +2,73 @@
  * Source https://github.com/donmahallem/js-libs Package: gulp-sharp
  */
 
+import * as  istanbul from 'istanbul';
+import * as libCoverage from 'istanbul-lib-coverage';
+import * as libReport from 'istanbul-lib-report';
+import * as reports from 'istanbul-reports';
+import { join as pathJoin } from 'path';
 import * as PluginError from 'plugin-error';
-import * as sharp from 'sharp';
 import { Transform } from 'stream';
 import * as through from 'through2';
 import * as VinylFile from 'vinyl';
-import { handleConfig, ISharpConfig } from './handle-config';
-import { handleMethod, SharpHandler } from './handle-method';
-
 const PLUGIN_NAME: string = '__BUILD_PACKAGE_NAME__';
-export interface IConfig {
-    config?: sharp.SharpOptions;
-    transform: ISharpConfig | SharpHandler;
+export interface IConfig<T = keyof reports.ReportOptions> {
+    /**
+     * Root dir
+     * Default: ./
+     */
+    rootDir?: string;
+    /**
+     * Dir to merge coverages into.
+     * Default: ./coverage
+     */
+    mergeDir?: string;
+    reports: Partial<reports.ReportOptions>;
 }
-const handleTransformPromise = (prom: Promise<VinylFile.BufferFile>, callback: through.TransformCallback): void => {
-    prom.then((convertedFile: VinylFile.BufferFile): void => {
-        callback(undefined, convertedFile);
-    }).catch((err: any): void => {
-        callback(new PluginError(PLUGIN_NAME, err, { message: 'Error while transforming file' }));
-    });
-};
-export const gulpSharp = (cfg: IConfig): Transform => {
+
+export const mergeCoverage = (cfg: IConfig): Transform => {
+    const rootDir: string = cfg.rootDir || './';
+    const mergeDir: string = cfg.mergeDir || './';
+    let mergedCoverageMap: libCoverage.CoverageMap;
+    const collector: istanbul.Collector = new istanbul.Collector();
+    const reporter: istanbul.Reporter = new istanbul.Reporter();
+
     return through.obj((file: VinylFile, encoding: BufferEncoding, callback: through.TransformCallback): void => {
         if (file.isNull()) {
             return callback(undefined, file);
         }
-
         if (file.isDirectory()) {
             return callback(new PluginError(PLUGIN_NAME, 'Directories are not supported'));
         } else if (file.isStream()) {
             return callback(new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
         } else if (file.isBuffer()) {
-            if (typeof (cfg.transform) === 'function') {
-                return handleTransformPromise(handleMethod(file, cfg.transform), callback);
+            const map: libCoverage.CoverageMap = libCoverage.createCoverageMap();
+            // tslint:disable-next-line:triple-equals
+            if (mergedCoverageMap != undefined) {
+                mergedCoverageMap.merge(map);
+            } else {
+                mergedCoverageMap = map;
             }
-            return handleTransformPromise(handleConfig(file, cfg.transform), callback);
+            collector.add(JSON.parse(file.contents?.toString()));
         }
+    }, (done: () => void): void => {
+
+        const context: libReport.Context = libReport.createContext({
+            coverageMap: mergedCoverageMap,
+            defaultSummarizer: 'nested',
+            dir: pathJoin(rootDir, mergeDir),
+        });
+        for (const key of Object.keys(cfg.reports)) {
+            const currentReporter: libReport.Visitor<libReport.Node> = reports.create(key as keyof reports.ReportOptions, cfg.reports[key]);
+
+            currentReporter.execute(context);
+        }
+        reporter.add('text');
+        reporter.addAll(['lcov', 'clover']);
+        reporter.write(collector, sync, function () {
+            console.log('All reports generated');
+        });
+
+        done();
     });
 };
