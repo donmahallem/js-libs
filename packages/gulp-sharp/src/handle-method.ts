698884f6a12948f230858b7a4080b6b247d198b3
donmahallem <donmahallem@users.noreply.github.com>
2020-08-17 12:35:25 +0200

    fix(gulp-sharp): use provided SharpOptions in handling (#30)
    * fix(gulp-sharp): add SharpOptions to handleConfig
    
    * fix(gulp-sharp): add SharpOptions to handleMethod
    
    * fix(gulp-sharp): provide SharpOptions to handle methods
    
    * chore(gulp-sharp): add test.svg image
    
    * test(gulp-sharp): add test cases for sharp input config

--- packages/gulp-sharp/src/handle-method.ts
+++ packages/gulp-sharp/src/handle-method.ts
@@ -6,7 +6,7 @@ import * as sharp from 'sharp';
 import { BufferFile } from 'vinyl';
 
 export type SharpHandler = (sh: sharp.Sharp) => sharp.Sharp;
-export const handleMethod = (inputFile: BufferFile, handler: SharpHandler): sharp.Sharp => {
-    const sharpInstance: sharp.Sharp = sharp(inputFile.contents);
+export const handleMethod = (inputFile: BufferFile, handler: SharpHandler, sharpInit?: sharp.SharpOptions): sharp.Sharp => {
+    const sharpInstance: sharp.Sharp = sharp(inputFile.contents, sharpInit);
     return handler(sharpInstance);
 };
