9915936bef912319341672e9bffba716fb4b9905
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-06 14:16:46 +0100

    chore(turbo): add RequestError

--- packages/turbo/src/index.ts
+++ packages/turbo/src/index.ts
@@ -1,5 +1,5 @@
 /*!
- * Source https://github.com/donmahallem/js-libs Package: gulp-sharp
+ * Source https://github.com/donmahallem/js-libs Package: turbo
  */
 
 export { RequestError } from './request-error';
--- packages/turbo/src/request-error.ts
+++ packages/turbo/src/request-error.ts
@@ -1,3 +1,7 @@
+/*!
+ * Source https://github.com/donmahallem/js-libs Package: turbo
+ */
+
 export class RequestError extends Error {
     constructor(message: string, public readonly code: number = 500) {
         super(message);
