fb7bfb851d231e06b0e740b5f0c13008ef3bdea8
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-08 13:39:35 +0100

    chore(turbo): add validateRequest to exports

--- packages/turbo/src/index.ts
+++ packages/turbo/src/index.ts
@@ -5,3 +5,4 @@
 export { formatResponse } from './format-response';
 export { promiseToResponse } from './promise-to-response';
 export { RequestError } from './request-error';
+export * from './validate-request';
