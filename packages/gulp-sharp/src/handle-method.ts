1c234de1ea94f6a38218447243e02ad0e88b625d
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-12 12:14:21 +0100

    docs(gulp-sharp): add docs to handleMethod

--- packages/gulp-sharp/src/handle-method.ts
+++ packages/gulp-sharp/src/handle-method.ts
@@ -6,6 +6,12 @@ import sharp from 'sharp';
 import { BufferFile } from 'vinyl';
 
 export type SharpHandler = (sh: sharp.Sharp) => sharp.Sharp;
+/**
+ * Sharp handler
+ * @param inputFile input file
+ * @param handler sharp handler
+ * @param sharpInit sharp options
+ */
 export const handleMethod = (inputFile: BufferFile, handler: SharpHandler, sharpInit?: sharp.SharpOptions): sharp.Sharp => {
     const sharpInstance: sharp.Sharp = sharp(inputFile.contents, sharpInit);
     return handler(sharpInstance);
