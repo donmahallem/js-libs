5493e63739a1c39a3ad22b9acd2ca75d06978d83
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-15 00:31:07 +0100

    fix(sync-gist): fix fs promises import

--- packages/sync-gist/src/load-file-content.ts
+++ packages/sync-gist/src/load-file-content.ts
@@ -2,14 +2,14 @@
  * Source https://github.com/donmahallem/js-libs Package: sync-gist
  */
 
-import { readFile } from 'fs/promises';
+import { promises as fsp } from 'fs';
 import { basename, resolve } from 'path';
 import { InputFileContent, IInputFile } from './types';
 
 export const loadFileContent: (file: IInputFile, cwd?: string) => Promise<InputFileContent>
     = async (file: IInputFile, cwd?: string): Promise<InputFileContent> => {
         const resolvedFilePath: string = cwd ? resolve(cwd, file.source) : resolve(file.source);
-        const fileContent: string = await readFile(resolvedFilePath, 'utf-8');
+        const fileContent: string = await fsp.readFile(resolvedFilePath, 'utf-8');
         return {
             content: fileContent,
             filename: basename(file.filename ? file.filename : resolvedFilePath),
