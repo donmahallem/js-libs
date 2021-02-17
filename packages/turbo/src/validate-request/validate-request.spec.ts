daf7f53806161476ff873d8e703d4a4e0fd71f65
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-17 20:56:42 +0100

    feat(turbo): make validateRequest less strict in typings

--- packages/turbo/src/validate-request/validate-request.spec.ts
+++ packages/turbo/src/validate-request/validate-request.spec.ts
@@ -2,19 +2,44 @@
  * Source https://github.com/donmahallem/js-libs Package: turbo
  */
 
+import { JSONSchemaType } from 'ajv';
 import { expect } from 'chai';
 import { RequestHandler } from 'express';
 import 'mocha';
 import { Done } from 'mocha';
 import { RequestError } from '../request-error';
-import { validateRequest, ValidationSchema } from './validate-request';
+import { validateRequest } from './validate-request';
 
+interface ITestObject {
+    bottom?: string;
+    top?: string;
+}
+const TEST_SCHEMA: JSONSchemaType<ITestObject> = {
+    properties: {
+        bottom: {
+            $id: 'bottom',
+            nullable: true,
+            pattern: '^[\\+\\-]?\\d+$',
+            type: 'string',
+        },
+        top: {
+            $id: 'top',
+            nullable: true,
+            pattern: '^[\\+\\-]?\\d+$',
+            type: 'string',
+        },
+    },
+    required: [],
+    type: 'object',
+};
 // tslint:disable:no-unused-expression
 describe('validate-request/validate-request.ts', (): void => {
     describe('validateRequest', (): void => {
         it('should pass if empty hash is provided', (done: Done): void => {
             const validationResult: RequestHandler = validateRequest('body', {
-                nullable: true,
+                properties: {
+
+                },
                 required: [],
                 type: 'object',
             });
@@ -25,25 +50,7 @@ describe('validate-request/validate-request.ts', (): void => {
         });
         describe(`should check 'query'`, (): void => {
             it(`should pass correctly`, (done: Done): void => {
-                const configObj: ValidationSchema<'query'> = {
-                    properties: {
-                        bottom: {
-                            $id: 'bottom',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                        top: {
-                            $id: 'top',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                    },
-                    required: [],
-                    type: 'object',
-                };
-                const validationResult: RequestHandler = validateRequest('query', configObj);
+                const validationResult: RequestHandler = validateRequest<ITestObject>('query', TEST_SCHEMA);
                 validationResult({
                     query: { top: '123', bottom: '-123' },
                 } as any, {} as any, (res?: any): void => {
@@ -52,25 +59,7 @@ describe('validate-request/validate-request.ts', (): void => {
                 });
             });
             it(`should reject property 'query' correctly`, (done: Done): void => {
-                const configObj: ValidationSchema<'query'> = {
-                    properties: {
-                        bottom: {
-                            $id: 'bottom',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                        top: {
-                            $id: 'top',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                    },
-                    required: [],
-                    type: 'object',
-                };
-                const validationResult: RequestHandler = validateRequest('query', configObj);
+                const validationResult: RequestHandler = validateRequest<ITestObject>('query', TEST_SCHEMA);
                 validationResult({
                     query: { top: 'asdf', bottom: '-123' },
                 } as any, {} as any, (res?: any): void => {
@@ -82,25 +71,7 @@ describe('validate-request/validate-request.ts', (): void => {
         });
         describe(`should check 'body'`, (): void => {
             it(`should pass correctly`, (done: Done): void => {
-                const configObj: ValidationSchema<'body'> = {
-                    properties: {
-                        bottom: {
-                            $id: 'bottom',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                        top: {
-                            $id: 'top',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                    },
-                    required: [],
-                    type: 'object',
-                };
-                const validationResult: RequestHandler = validateRequest('body', configObj);
+                const validationResult: RequestHandler = validateRequest<ITestObject>('body', TEST_SCHEMA);
                 validationResult({
                     body: { top: '123', bottom: '-123' },
                 } as any, {} as any, (res?: any): void => {
@@ -109,25 +80,7 @@ describe('validate-request/validate-request.ts', (): void => {
                 });
             });
             it(`should reject property 'body' correctly`, (done: Done): void => {
-                const configObj: ValidationSchema<'body'> = {
-                    properties: {
-                        bottom: {
-                            $id: 'bottom',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                        top: {
-                            $id: 'top',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                    },
-                    required: [],
-                    type: 'object',
-                };
-                const validationResult: RequestHandler = validateRequest('body', configObj);
+                const validationResult: RequestHandler = validateRequest<ITestObject>('body', TEST_SCHEMA);
                 validationResult({
                     body: { top: 'asdf', bottom: '-123' },
                 } as any, {} as any, (res?: any): void => {
@@ -139,25 +92,7 @@ describe('validate-request/validate-request.ts', (): void => {
         });
         describe(`should check 'params'`, (): void => {
             it(`should pass correctly`, (done: Done): void => {
-                const configObj: ValidationSchema<'params'> = {
-                    properties: {
-                        bottom: {
-                            $id: 'bottom',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                        top: {
-                            $id: 'top',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                    },
-                    required: [],
-                    type: 'object',
-                };
-                const validationResult: RequestHandler = validateRequest('params', configObj);
+                const validationResult: RequestHandler = validateRequest<ITestObject>('params', TEST_SCHEMA);
                 validationResult({
                     params: { top: '123', bottom: '-123' },
                 } as any, {} as any, (res?: any): void => {
@@ -166,25 +101,7 @@ describe('validate-request/validate-request.ts', (): void => {
                 });
             });
             it(`should reject property 'params' correctly`, (done: Done): void => {
-                const configObj: ValidationSchema<'params'> = {
-                    properties: {
-                        bottom: {
-                            $id: 'bottom',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                        top: {
-                            $id: 'top',
-                            nullable: true,
-                            pattern: '^[\\+\\-]?\\d+$',
-                            type: 'string',
-                        },
-                    },
-                    required: [],
-                    type: 'object',
-                };
-                const validationResult: RequestHandler = validateRequest('params', configObj);
+                const validationResult: RequestHandler = validateRequest<ITestObject>('params', TEST_SCHEMA);
                 validationResult({
                     params: { top: 'asdf', bottom: '-123' },
                 } as any, {} as any, (res?: any): void => {
--- packages/turbo/src/validate-request/validate-request.ts
+++ packages/turbo/src/validate-request/validate-request.ts
@@ -6,19 +6,20 @@ import Ajv, { DefinedError, JSONSchemaType, ValidateFunction } from 'ajv';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { convertValidationError } from './convert-validation-error';
 
-type RequestSignature = Partial<Pick<Request, 'query' | 'params' | 'body'>>;
-export type RequestSchema = JSONSchemaType<RequestSignature, true> & { additionalProperties: true };
-
 type CheckKeys = 'query' | 'params' | 'body';
-export type ValidationSchema<T extends CheckKeys, P extends boolean = false> = JSONSchemaType<Request[T], P>;
+type MethodSignature = < T = object, _partial extends boolean = false >(key: CheckKeys,
+    schema: JSONSchemaType<T, _partial>,
+    ajvInstance?: Ajv) => RequestHandler;
 /**
  * Checks a request against the given schema
  * @param key property of the request to check the schema against
  * @param schema the jsonschema
  * @param ajvInstance optional ajv to be used
  */
-export const validateRequest: <K extends CheckKeys>(key: K, schema: ValidationSchema<K>, ajvInstance?: Ajv) => RequestHandler =
-    <K extends CheckKeys>(key: K, schema: ValidationSchema<K>, ajvInstance: Ajv = new Ajv()): RequestHandler => {
+export const validateRequest: MethodSignature =
+    <T, _partial extends boolean = false>(key: CheckKeys,
+        schema: JSONSchemaType<T, _partial>,
+        ajvInstance: Ajv = new Ajv()): RequestHandler => {
         const validateFunction: ValidateFunction = ajvInstance.compile(schema);
         return (req: Request, res: Response, next: NextFunction): void => {
             if (!validateFunction(req[key])) {
