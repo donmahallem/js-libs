bffa16a06b8a14db1900441ba610c730ca405c55
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-19 13:19:41 +0100

    feat(turbo-pb): add turbo-pb package

---
+++ packages/turbo-pb/src/format-response.spec.ts
@@ -0,0 +1,99 @@
+/*!
+ * Source https://github.com/donmahallem/js-libs Package: turbo
+ */
+
+import { expect } from 'chai';
+import express from 'express';
+import 'mocha';
+import * as protobuf from 'protobufjs';
+import * as sinon from 'sinon';
+import supertest from 'supertest';
+import { formatResponse } from './format-response';
+interface ITestMessage {
+    message: string;
+}
+
+const protoRoot: protobuf.Root = protobuf.loadSync('./test/test.proto');
+const testMessage: protobuf.Type = protoRoot.lookupType('testpackage.TestMessage');
+const testObject: ITestMessage = { message: 'test' };
+const encodedTestMessage: Buffer = Buffer.from(testMessage.encode(testObject).finish());
+describe('./format-response', (): void => {
+    let sandbox: sinon.SinonSandbox;
+    let app: express.Application;
+    let getResponseBody: sinon.SinonStub;
+    let errorSpy: sinon.SinonSpy;
+    before((): void => {
+        sandbox = sinon.createSandbox();
+        getResponseBody = sandbox.stub();
+        errorSpy = sandbox.spy();
+    });
+    beforeEach((): void => {
+        app = express();
+        app.use((req: express.Request, res: express.Response, next: express.NextFunction): void => {
+            formatResponse(getResponseBody(), testMessage, res, next);
+        });
+        app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction): void => {
+            errorSpy(err);
+            res
+                .status(406)
+                .json({
+                    message: err.message,
+                });
+        });
+    });
+    afterEach((): void => {
+        sandbox.reset();
+    });
+    after((): void => {
+        sandbox.restore();
+    });
+    it('validate protobuf test message', (): void => {
+        const encoded: Uint8Array = testMessage.encode(testObject).finish();
+        expect(testMessage.decode(encoded).toJSON()).to.deep.eq(testObject);
+    });
+    describe('formatResponse', (): void => {
+        describe('protobuf response', (): void => {
+            it('validate protobuf test message', async (): Promise<void> => {
+                getResponseBody.returns(testObject);
+                return supertest(app)
+                    .get('')
+                    .set('Accept', 'application/x-protobuf')
+                    .expect(200)
+                    .expect('Content-Type', 'application/x-protobuf')
+                    .responseType('blob')
+                    .then((response: any): void => {
+                        expect(response.body).to.deep.equal(encodedTestMessage);
+                    });
+            });
+        });
+        describe('json response', (): void => {
+            it('validate json test message', async (): Promise<void> => {
+                getResponseBody.returns(testObject);
+                return supertest(app)
+                    .get('')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .then((response: any): void => {
+                        expect(response.body).to.deep.equal(testObject);
+                    });
+            });
+        });
+        describe('any other response', (): void => {
+            it('should respond with error', async (): Promise<void> => {
+                getResponseBody.returns(testObject);
+                return supertest(app)
+                    .get('')
+                    .set('Accept', 'application/pdf')
+                    .expect(406)
+                    .expect('Content-Type', /json/)
+                    .then((response: any): void => {
+                        expect(errorSpy.callCount).to.equal(1, 'only one error should be emitted');
+                        expect(response.body).to.deep.equal({
+                            message: 'Not Acceptable',
+                        });
+                    });
+            });
+        });
+    });
+});
---
+++ packages/turbo-pb/src/index.ts
@@ -0,0 +1,5 @@
+/*!
+ * Source https://github.com/donmahallem/js-libs Package: turbo-pb
+ */
+
+export { formatResponse } from './format-response';
