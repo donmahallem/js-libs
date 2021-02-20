d00f57fa1ed97061a15a0dd97e669bc679fe2933
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-20 14:49:35 +0100

    chore: internally use promiseToResponse

--- packages/turbo/src/promise-request-handler.ts
+++ packages/turbo/src/promise-request-handler.ts
@@ -2,10 +2,11 @@
  * Source https://github.com/donmahallem/js-libs Package: turbo
  */
 
-import { AxiosError } from 'axios';
 import { NextFunction, RequestHandler, Response, Request } from 'express';
+import { IErrorResponse } from './error-response';
+import { promiseToResponse } from './promise-to-response';
 
-type MethodInterface<T> = (req?: Request, res?: Response) => Promise<T>;
+type MethodInterface<T> = (req?: Request, res?: Response<T>) => Promise<T>;
 /**
  * Awaits an promise and returns it
  * @param prom promise to convert
@@ -14,29 +15,7 @@ type MethodInterface<T> = (req?: Request, res?: Response) => Promise<T>;
  */
 export const promiseRequestHandler: <T>(prom: MethodInterface<T>) => RequestHandler =
     <T>(prom: MethodInterface<T>): RequestHandler => {
-        return (req: Request, res: Response<T | any>, next: NextFunction): void => {
-            prom(req, res)
-                .then((value: T): void => {
-                    res.status(200).json(value);
-                })
-                .catch((err: any | AxiosError): void => {
-                    if (next) {
-                        next(err);
-                    } else if (err.isAxiosError === true) {
-                        const axiosError: AxiosError = err;
-                        const code: number = axiosError.response?.status || 500;
-                        res.status(code)
-                            .json({
-                                error: true,
-                                statusCode: code,
-                            });
-                    } else {
-                        const code: number = err.statusCode || 500;
-                        res.status(code).json({
-                            error: true,
-                            statusCode: code,
-                        });
-                    }
-                });
+        return (req: Request, res: Response<T | IErrorResponse>, next: NextFunction): void => {
+            promiseToResponse(prom(req, res), res, next);
         }
     };
