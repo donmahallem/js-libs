199cbce462cac4e09d88b58c390e5b4873cf8b08
donmahallem <donmahallem@users.noreply.github.com>
2021-02-23 13:53:52 +0100

    feat(common-rollup): add common-rollup package (#361)
    * feat(common-rollup): add common-rollup package
    
    * test(rollup-config): add test case
    
    * chore(deps): add rollup peer dependency
    
    * chore(deps): clean up package-lock
    
    * style(rollup-config): fix lint issues

---
+++ packages/rollup-config/src/index.ts
@@ -0,0 +1,51 @@
+/**!
+ * Source https://github.com/donmahallem/js-libs Package: rollup-config
+ */
+
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+
+export default (pkg: any): any => {
+    const output: any[] = [];
+    if (pkg.main) {
+        output.push({
+            file: pkg.main,
+            footer: '// BUILD: __BUILD_DATE__\n\n',
+            format: 'cjs',
+            preferConst: true,
+            sourcemap: true,
+        });
+    }
+    if (pkg.module) {
+        output.push({
+            file: pkg.module,
+            footer: '// BUILD: __BUILD_DATE__\n\n',
+            format: 'esm',
+            preferConst: true,
+            sourcemap: true,
+        });
+    }
+    return {
+        external: [
+            ...Object.keys(pkg.dependencies || {}),
+            ...Object.keys(pkg.devDependencies || {}),
+            ...Object.keys(pkg.peerDependencies || {}),
+            ...Object.keys(pkg.optionalDependencies || {}),
+        ],
+        input: 'src/index.ts',
+        output,
+        plugins: [
+            nodeResolve(),
+            typescript({
+                tsconfig: './tsconfig.json',
+            }),
+            replace({
+                __BUILD_DATE__: (): string => new Date().toString(),
+                __BUILD_PACKAGE_NAME__: pkg.name,
+                __BUILD_VERSION__: pkg.version,
+            }),
+        ],
+        preserveSymlinks: true,
+    };
+};
