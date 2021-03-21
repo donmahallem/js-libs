a82d8956fe5cf3a5f4156450496629b82a7c116c
ò_ó <donmahallem@users.noreply.github.com>
2021-03-21 18:45:53 +0100

    feat(label-gh): add label-gh package (#457)
    * chore(label-pr): init repo
    
    * feat(label-pr): add calculateLabelDiff
    
    * feat(label-pr): add setLabel method
    
    * feat(label-pr): add addLabel method
    
    * feat(label-pr): add syncPrLabel
    
    * chore(label-pr): add several auxilary methods
    
    * style(label-pr): fix lint issues
    
    * chore(label-pr): add package-lock.json
    
    * test(label-pr): add addLabels tests
    
    * test(label-pr): add setLabels tests
    
    * chore(label-gh): rename package label-pr to label-gh
    
    * refactor(label-gh): rename syncPrLabels to syncLabels
    
    * fix(label-gh): fix export naming
    
    * chore(label-gh): remove async keyword
    
    * test(label-gh): add syncLabels test cases
    
    * test(label-gh): add rejection test to setLabels
    
    * test(label-gh): add rejection test to addLabels

---
+++ packages/label-gh/src/add-labels.ts
@@ -0,0 +1,17 @@
+/*!
+ * Source https://github.com/donmahallem/js-libs Package: label-gh
+ */
+
+import { Octokit } from '@octokit/core';
+import { Endpoints } from '@octokit/types';
+
+type PostLabelParameters = Endpoints['POST /repos/{owner}/{repo}/issues/{issue_number}/labels']['parameters'];
+type PostLabelResponse = Endpoints['POST /repos/{owner}/{repo}/issues/{issue_number}/labels']['response'];
+export const addLabels = async (octokit: Octokit,
+  params: PostLabelParameters): Promise<PostLabelResponse['data']> => {
+  return octokit
+    .request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', params)
+    .then((resp: PostLabelResponse): PostLabelResponse['data'] => {
+      return resp.data;
+    });
+};
