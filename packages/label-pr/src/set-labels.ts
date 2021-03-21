fe795e7b39b2cc5a686113209512c0ccba466ed6
DonMahallem <donmahallem@users.noreply.github.com>
2021-03-21 10:42:15 +0100

    feat(label-pr): add setLabel method

---
+++ packages/label-pr/src/set-labels.ts
@@ -0,0 +1,14 @@
+import { Octokit } from "@octokit/core"
+import { Endpoints } from "@octokit/types";
+
+type PutLabelParameters = Endpoints['PUT /repos/{owner}/{repo}/issues/{issue_number}/labels']['parameters'];
+type PutLabelResponse = Endpoints['PUT /repos/{owner}/{repo}/issues/{issue_number}/labels']['response'];
+export type ResponseData = PutLabelResponse['data'];
+export const setLabels = async (octokit: Octokit,
+  params: PutLabelParameters): Promise<ResponseData> => {
+  return await octokit
+    .request('PUT /repos/{owner}/{repo}/issues/{issue_number}/labels', params)
+    .then((resp: PutLabelResponse): ResponseData => {
+      return resp.data;
+    });
+}
