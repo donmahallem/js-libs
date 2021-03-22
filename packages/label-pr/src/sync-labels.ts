4b8a4f2744b062f8e7fdeeb41fd27fe8f99dc38f
DonMahallem <donmahallem@users.noreply.github.com>
2021-03-22 08:47:20 +0100

    chore(label-pr): save

--- packages/label-pr/src/sync-labels.ts
+++ packages/label-pr/src/sync-labels.ts
@@ -2,15 +2,16 @@
  * Source https://github.com/donmahallem/js-libs Package: label-gh
  */
 
+import { calculateLabelDiff, syncLabels } from '@donmahallem/label-gh';
 import { Octokit } from '@octokit/core';
-import { addLabels } from './add-labels';
-import { setLabels, ResponseData } from './set-labels';
+import { PRLabel } from './github-types';
+import { getPullRequestLabels } from './pull-request-labels';
 
 export interface IOpts {
     /**
      * Issue or pull request number
      */
-    issue_number: number;
+    pull_number: number;
     /**
      * Repository owner
      */
@@ -20,19 +21,23 @@ export interface IOpts {
      */
     repo: string;
 }
-export const syncLabels = (octokit: Octokit,
+export const syncPackageLabels = async (octokit: Octokit,
     opts: IOpts,
-    labels: string[],
-    replace: boolean = true): Promise<ResponseData> => {
-    if (replace) {
-        return setLabels(octokit, {
-            ...opts,
-            labels,
-        });
-    } else {
-        return addLabels(octokit, {
-            ...opts,
-            labels,
-        });
-    }
+    packageLabel: string[],
+    prefix: string = 'pkg'): Promise<ResponseData> => {
+    const expectedLabels: string[] = packageLabel.map((baseLabel: string): string => {
+        return `${prefix}:${baseLabel}`;
+    })
+    const prLabels: PRLabel[] = await getPullRequestLabels(octokit, opts);
+    const prLabelNames: (string | undefined)[] = prLabels.map((lab: PRLabel): string | undefined => {
+        return lab.name;
+    });
+    const filteredPrLabels: string[] = prLabelNames.filter((label: string | undefined): boolean => {
+        if (label) {
+            return label.startsWith(`${prefix}:`);
+        }
+        return false;
+    }) as string[];
+    const diff: ILabelDiff = calculateLabelDiff(expectedLabels, filteredPrLabels);
+    await syncLabels(octokit, opts, diff.)
 };
