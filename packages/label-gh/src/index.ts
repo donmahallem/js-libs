7820388d30278e888067d7089f1ed2caf7f7897a
ò_ó <donmahallem@users.noreply.github.com>
2021-03-22 11:49:12 +0100

    fix(label-gh): export ILabelDiff interface (#468)

--- packages/label-gh/src/index.ts
+++ packages/label-gh/src/index.ts
@@ -2,5 +2,5 @@
  * Source https://github.com/donmahallem/js-libs Package: label-gh
  */
 
-export { calculateLabelDiff } from './calculate-label-diff';
+export { calculateLabelDiff, ILabelDiff } from './calculate-label-diff';
 export { syncLabels } from './sync-labels';
