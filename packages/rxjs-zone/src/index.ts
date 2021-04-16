d49d958ea53a3aa7f49dfddd81f0ab1ca02d351e
ò_ó <donmahallem@users.noreply.github.com>
2021-04-16 10:46:11 +0200

    feat(rxjs-zone): adds rxjs-zone package (#532)
    adds a package to help with rxjs with handling zone.js

---
+++ packages/rxjs-zone/src/index.ts
@@ -0,0 +1,6 @@
+/*!
+ * Source https://github.com/donmahallem/js-libs Package: rxjs-zone
+ */
+
+export { runInsideZone } from './run-inside-zone';
+export { runOutsideZone } from './run-outside-zone';
---
+++ packages/rxjs-zone/src/run-outside-zone.ts
@@ -0,0 +1,29 @@
+/*!
+ * Source https://github.com/donmahallem/js-libs Package: rxjs-zone
+ */
+
+import { NgZone } from '@angular/core';
+import {
+    MonoTypeOperatorFunction,
+    Observable,
+    Subscriber,
+    Subscription,
+} from 'rxjs';
+
+export const runOutsideZone: <T>(zone: NgZone) => MonoTypeOperatorFunction<T> =
+    <T>(zone: NgZone): MonoTypeOperatorFunction<T> =>
+        (source: Observable<T>): Observable<T> =>
+            new Observable<T>((observer: Subscriber<T>): Subscription =>
+                source.subscribe({
+                    next(x: T): void {
+                        if (NgZone.isInAngularZone()) {
+                            zone.runOutsideAngular((): void => {
+                                observer.next(x);
+                            });
+                        } else {
+                            observer.next(x);
+                        }
+                    },
+                    error(err: any): void { observer.error(err); },
+                    complete(): void { observer.complete(); },
+                }));
