cd2ad1ea7a910a9d6ac2445aea0c5bea2b9f29e8
DonMahallem <donmahallem@users.noreply.github.com>
2021-07-23 23:56:51 +0200

    chore: fix scripts

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -20,7 +20,7 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
-        "plugin:@typescript-eslint/eslint-recommended",
+        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier',
