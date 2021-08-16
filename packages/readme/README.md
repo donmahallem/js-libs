1ef6a25ff4ac60ac401970cfe060113ff49389ee
DonMahallem <donmahallem@users.noreply.github.com>
2021-08-17 00:54:23 +0200

    chore: save

---
+++ packages/readme/README.md
@@ -0,0 +1,27 @@
+# {{ template:title }}
+
+{{ template:badges }}
+
+{{ template:description }}
+
+## Toc
+
+*   [Install](#install)
+*   [Contributors](#contributors)
+*   [License](#license)
+
+## Install
+
+Run the following command to install the package:
+
+    npm install {{ pkg.name }}
+
+## Contributors
+
+| Name            | GitHub                                             |
+| :-------------- | :------------------------------------------------- |
+| **donmahallem** | [**@donmahallem**](https://github.com/donmahallem) |
+
+## License
+
+[Apache-2.0](LICENSE) © [donmahallem](https://github.com/donmahallem)
--- packages/readme/src/index.ts
+++ packages/readme/src/index.ts
@@ -3,26 +3,54 @@
  * Source https://donmahallem.github.io/js-libs/
  */
 
-
-import { remark } from 'remark'
+import { Command as cmd } from 'commander';
+import { promises as fsp } from 'fs';
+import { remark } from 'remark';
 import remarkGfm from 'remark-gfm';
-import remarkGitContributors from 'remark-gfm';
-import remarkHtml from 'remark-html'
+import remarkGitContributors from 'remark-git-contributors';
 import remarkLicense from 'remark-license';
 import remarkParse from 'remark-parse';
 import remarkToc from 'remark-toc';
-import { reporter } from 'vfile-reporter'
+import { read } from 'to-vfile';
+import { VFile } from 'vfile';
+import { reporter } from 'vfile-reporter';
+async function run(opts: { dryRun: boolean; input: string; output?: string }): Promise<void> {
+    const output: string = opts.output || opts.input;
+    console.log('Updating readme with', opts.input, '=>', output);
+    const data: VFile = await read(opts.input);
+    return (
+        remark()
+            .use(remarkToc)
+            .use(remarkGfm)
+            .use(remarkParse)
+            .use(remarkGitContributors)
+            .use(remarkLicense)
+            // .use(remarkPresetLintRecommended)
+            .process(data)
+            .then((file: VFile): Promise<void> | void => {
+                console.error(reporter(file));
+                if (opts.dryRun) {
+                    console.log(String(file));
+                } else {
+                    return fsp.writeFile(output, String(file), 'utf-8');
+                }
+            })
+    );
+}
 
-remark()
-    .use(remarkToc)
-    .use(remarkGfm)
-    .use(remarkParse)
-    .use(remarkGitContributors)
-    .use(remarkLicense)
-    // .use(remarkPresetLintRecommended)
-    .use(remarkHtml)
-    .process('## Hello world!')
-    .then((file) => {
-        console.error(reporter(file))
-        console.log(String(file))
+async function main() {
+    const prg = new cmd()
+        .version('0.0.1')
+        .requiredOption('-i, --input <type>', 'input file required')
+        .option('-o, --output <type>', 'output file required')
+        .option('-d, --dry-run <dryrun>', 'run in dry-run mode', false)
+        .description('Converts <input> markdown to <output> markdown')
+        .action(run);
+    await prg.parseAsync(process.argv);
+}
+
+main()
+    .then((): void => {
+        console.log('File converted');
     })
+    .catch(console.error);
