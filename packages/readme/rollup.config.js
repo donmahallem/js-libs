372a9c3b3e2c91488f8ee03e401d85096b617c44
DonMahallem <donmahallem@users.noreply.github.com>
2021-08-21 23:16:30 +0200

    fix(readme): build missing cli.js
    fixes #779

--- packages/readme/rollup.config.js
+++ packages/readme/rollup.config.js
@@ -1,4 +1,18 @@
 import defaultRollup from '@donmahallem/rollup-config';
 import pkg from './package.json';
 
-export default defaultRollup(pkg);
+const defaultConfig = defaultRollup(pkg);
+export default Object.assign(defaultConfig, {
+    input: {
+        index: 'src/index.ts',
+        cli: 'src/cli.ts',
+    },
+    output: defaultConfig
+        .output
+        .map((item) => {
+            return Object.assign(item, {
+                file: null,
+                dir: './dist/esm',
+            });
+        }),
+});
