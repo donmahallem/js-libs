c769ecf2710397d43fe79b2f089c5b0c8397cbd1
DonMahallem <donmahallem@users.noreply.github.com>
2021-09-01 09:50:55 +0200

    style(gulp-sharp): fix eslint issues

--- packages/gulp-sharp/src/sharp-to-vinyl-buffer.ts
+++ packages/gulp-sharp/src/sharp-to-vinyl-buffer.ts
@@ -1,5 +1,6 @@
-/*!
- * Source https://github.com/donmahallem/js-libs Package: gulp-sharp
+/*
+ * Package @donmahallem/gulp-sharp
+ * Source https://donmahallem.github.io/js-libs/
  */
 
 import { basename, extname } from 'path';
@@ -10,22 +11,24 @@ import { IConfig } from './config';
 
 /**
  * Converts a provided bufferfile with sharp
+ *
  * @param input sharp instance
  * @param sourceFile input file
  * @param cfg sharp config
  */
-export const sharpToVinylBuffer: (input: Sharp, sourceFile: BufferFile, cfg: IConfig) => Promise<BufferFile> =
-    (input: Sharp, sourceFile: BufferFile, cfg: IConfig): Promise<BufferFile> => {
-        return input
-            .toBuffer({ resolveWithObject: true })
-            .then((data: IBufferResponse): BufferFile => {
-                const newFile: BufferFile = sourceFile.clone({ contents: false });
-                newFile.contents = data.data;
-                if (cfg.modifyFilename !== false) {
-                    const fileBasename: string = basename(sourceFile.basename, extname(sourceFile.basename));
-                    newFile.basename = `${fileBasename}_${data.info.width}w_${data.info.height}h`;
-                }
-                newFile.extname = `.${data.info.format}`;
-                return newFile;
-            });
-    };
+export const sharpToVinylBuffer: (input: Sharp, sourceFile: BufferFile, cfg: IConfig) => Promise<BufferFile> = (
+    input: Sharp,
+    sourceFile: BufferFile,
+    cfg: IConfig
+): Promise<BufferFile> => {
+    return input.toBuffer({ resolveWithObject: true }).then((data: IBufferResponse): BufferFile => {
+        const newFile: BufferFile = sourceFile.clone({ contents: false });
+        newFile.contents = data.data;
+        if (cfg.modifyFilename !== false) {
+            const fileBasename: string = basename(sourceFile.basename, extname(sourceFile.basename));
+            newFile.basename = `${fileBasename}_${data.info.width}w_${data.info.height}h`;
+        }
+        newFile.extname = `.${data.info.format}`;
+        return newFile;
+    });
+};
