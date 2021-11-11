50a4036fd40bdbadf27d7aef767a8fc3312769d8
ò_ó <donmahallem@users.noreply.github.com>
2021-11-12 00:26:57 +0100

    feat(rollup-config): add rollup typings for export (#993)

--- packages/rollup-config/src/index.spec.ts
+++ packages/rollup-config/src/index.spec.ts
@@ -5,20 +5,20 @@
 
 import { expect } from 'chai';
 import 'mocha';
+import { RollupOptions } from 'rollup';
 import defaultConfig from './index';
 
-/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
 describe('index', (): void => {
     it('should set all plugins with default config', (): void => {
-        const result: any = defaultConfig({ name: 'testname', version: '1.2.3' });
+        const result: RollupOptions = defaultConfig({ name: 'testname', version: '1.2.3' });
         expect(result.plugins).to.have.lengthOf(5);
     });
     it('should exclude commonjs with false as config', (): void => {
-        const result: any = defaultConfig({ name: 'testname', version: '1.2.3' }, { plugins: { commonjs: false } });
+        const result: RollupOptions = defaultConfig({ name: 'testname', version: '1.2.3' }, { plugins: { commonjs: false } });
         expect(result.plugins).to.have.lengthOf(4);
     });
     it('should exclude commonjs with false as config', (): void => {
-        const result: any = defaultConfig({ name: 'testname', version: '1.2.3' }, { plugins: { json: false } });
+        const result: RollupOptions = defaultConfig({ name: 'testname', version: '1.2.3' }, { plugins: { json: false } });
         expect(result.plugins).to.have.lengthOf(4);
     });
 });
