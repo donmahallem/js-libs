696cf973b49385042c17dcf4e02296f7b9b4f111
DonMahallem <donmahallem@users.noreply.github.com>
2021-11-11 23:24:55 +0100

    style(rxjs-zone): fix lint issues

--- packages/rxjs-zone/src/index.spec.ts
+++ packages/rxjs-zone/src/index.spec.ts
@@ -1,5 +1,6 @@
-/*!
- * Source https://github.com/donmahallem/js-libs Package: rxjs-zone
+/*
+ * Package @donmahallem/rxjs-zone
+ * Source https://donmahallem.github.io/js-libs/
  */
 
 import { expect } from 'chai';
