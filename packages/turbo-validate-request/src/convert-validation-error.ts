7dd1fb7b6e47857b2d4d2d29ec3b393985e211d1
DonMahallem <donmahallem@users.noreply.github.com>
2021-12-09 19:36:47 +0100

    style(turbo-validate-request): replace tslint with eslint based linter

--- packages/turbo-validate-request/src/convert-validation-error.ts
+++ packages/turbo-validate-request/src/convert-validation-error.ts
@@ -1,21 +1,21 @@
-/*!
- * Source https://github.com/donmahallem/js-libs Package: turbo-validate-request
+/*
+ * Package @donmahallem/turbo-validate-request
+ * Source https://donmahallem.github.io/js-libs/
  */
 
 import { RequestError } from '@donmahallem/turbo';
 import { DefinedError } from 'ajv';
 
-export const convertValidationError: (error: DefinedError) => RequestError =
-    (error: DefinedError): RequestError => {
-        const errorPath: string = error.instancePath === '' ? 'root' : error.instancePath;
-        switch (error.keyword) {
-            case 'required':
-                return new RequestError(`Missing property ${error.params.missingProperty} at '${errorPath}'`, 400);
-            case 'pattern':
-                return new RequestError(`Value doesn't match pattern at: '${errorPath}'`, 400);
-            case 'type':
-                return new RequestError(`Invalid type at '${errorPath}'. Expected ${error.params.type}`, 400);
-            default:
-                return new RequestError(`Invalid '${errorPath}'`, 400);
-        }
-    };
+export const convertValidationError: (error: DefinedError) => RequestError = (error: DefinedError): RequestError => {
+    const errorPath: string = error.instancePath === '' ? 'root' : error.instancePath;
+    switch (error.keyword) {
+        case 'required':
+            return new RequestError(`Missing property ${error.params.missingProperty} at '${errorPath}'`, 400);
+        case 'pattern':
+            return new RequestError(`Value doesn't match pattern at: '${errorPath}'`, 400);
+        case 'type':
+            return new RequestError(`Invalid type at '${errorPath}'. Expected ${error.params.type}`, 400);
+        default:
+            return new RequestError(`Invalid '${errorPath}'`, 400);
+    }
+};
--- packages/turbo-validate-request/src/validate-request.ts
+++ packages/turbo-validate-request/src/validate-request.ts
@@ -1,5 +1,6 @@
-/*!
- * Source https://github.com/donmahallem/js-libs Package: turbo-validate-request
+/*
+ * Package @donmahallem/turbo-validate-request
+ * Source https://donmahallem.github.io/js-libs/
  */
 
 import Ajv, { DefinedError, JSONSchemaType, ValidateFunction } from 'ajv';
@@ -7,26 +8,27 @@ import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { convertValidationError } from './convert-validation-error';
 
 type CheckKeys = 'query' | 'params' | 'body';
-type MethodSignature = < T = object>(key: CheckKeys,
-    schema: JSONSchemaType<T>,
-    ajvInstance?: Ajv) => RequestHandler;
+type MethodSignature = <T = object>(key: CheckKeys, schema: JSONSchemaType<T>, ajvInstance?: Ajv) => RequestHandler;
 /**
  * Checks a request against the given schema
- * @param key property of the request to check the schema against
- * @param schema the jsonschema
- * @param ajvInstance optional ajv to be used
+ *
+ * @param {string} key property of the request to check the schema against
+ * @param {JSONSchemaType} schema the jsonschema
+ * @param {Ajv} ajvInstance optional ajv to be used
+ * @returns {RequestHandler} express middleware
  */
-export const validateRequest: MethodSignature =
-    <T, _partial extends boolean = false>(key: CheckKeys,
-        schema: JSONSchemaType<T>,
-        ajvInstance: Ajv = new Ajv()): RequestHandler => {
-        const validateFunction: ValidateFunction = ajvInstance.compile(schema);
-        return (req: Request, res: Response, next: NextFunction): void => {
-            if (!validateFunction(req[key])) {
-                const errors: DefinedError[] = validateFunction.errors as DefinedError[];
-                next(convertValidationError(errors[0] as DefinedError));
-                return;
-            }
-            next();
-        };
+export const validateRequest: MethodSignature = <T>(
+    key: CheckKeys,
+    schema: JSONSchemaType<T>,
+    ajvInstance: Ajv = new Ajv()
+): RequestHandler => {
+    const validateFunction: ValidateFunction = ajvInstance.compile(schema);
+    return (req: Request, res: Response, next: NextFunction): void => {
+        if (!validateFunction(req[key])) {
+            const errors: DefinedError[] = validateFunction.errors as DefinedError[];
+            next(convertValidationError(errors[0]));
+            return;
+        }
+        next();
     };
+};
