f09481d3504c32aa4e1c518acf0f7ddd1d6c6793
ò_ó <donmahallem@users.noreply.github.com>
2022-01-02 20:41:11 +0100

    feat(lerna2codecov): add lerna2codecov package (#1130)
    * chore(lerna2codecov): setup lerna2codecov package
    
    * chore(lerna2codecov): reset package version to 0.0.0
    
    * style(lerna2codecov): fix lint issues
    
    * style(lerna2codecov): fix lint issues

---
+++ packages/lerna2codecov/src/lerna-project.d.ts
@@ -0,0 +1,113 @@
+/*
+ * Package @donmahallem/lerna2codecov
+ * Source https://github.com/donmahallem/js-libs/tree/master/packages/lerna2codecov
+ */
+
+/* eslint-disable */
+declare module '@lerna/package' {
+    import { resolve } from 'npm-package-arg';
+    type ResolveResult = ReturnType<typeof resolve>;
+    export type RawManifest = {
+        name: string;
+        version: string;
+        private?: boolean;
+        bin?: Record<string, string> | string;
+        scripts?: Record<string, string>;
+        dependencies?: Record<string, string>;
+        devDependencies?: Record<string, string>;
+        optionalDependencies?: Record<string, string>;
+        peerDependencies?: Record<string, string>;
+        publishConfig?: Record<string, string>;
+        workspaces?: string[] | { packages: string[] };
+    } & { [key: string]: any };
+    /**
+     * Lerna's internal representation of a local package, with
+     * many values resolved directly from the original JSON.
+     */
+    export class Package {
+        public static lazy(ref: string | Package | RawManifest, dir?: string): Package;
+        public constructor(pkg: RawManifest, location: string, rootPath?: string);
+        public get location(): string;
+        public get private(): boolean;
+        public get resolved(): ResolveResult;
+        public get rootPath(): string;
+        public get scripts(): RawManifest['scripts'];
+        public get bin(): RawManifest['scripts'];
+        public get binLocation(): string;
+        public get manifestLocation(): string;
+        public get nodeModuleLocation(): string;
+        public get __isLernaPackage(): boolean;
+        public get version(): string;
+        public set version(v: string);
+        public get contents(): string;
+        public set contents(subDirectory: string);
+        public get dependencies(): RawManifest['dependencies'];
+        public get devDependencies(): RawManifest['devDependencies'];
+        public get optionalDependencies(): RawManifest['optionalDependencies'];
+        public get peerDependencies(): RawManifest['peerDependencies'];
+        get<K extends keyof RawManifest | string>(key: K): RawManifest[K];
+        set<K extends keyof RawManifest | string>(key: K, val: RawManifest[K]): void;
+        /**
+         * Provide shallow copy for munging elsewhere
+         */
+        public toJson(): Object;
+
+        /**
+         * Refresh internal state from disk (e.g., changed by external lifecycles)
+         */
+        public refresh(): Promise<Package>;
+        /**
+         * Write manifest changes to disk
+         */
+        public serialize(): Promise<void>;
+        /**
+         * Mutate local dependency spec according to type
+         */
+        public updateLocalDependency(resolved: ResolveResult, depVersion: string, savePrefix: string): void;
+    }
+}
+
+declare module '@lerna/project' {
+    import { Package } from '@lerna/package';
+
+    export interface ProjectConfig {
+        packages: string[];
+        useWorkspaces: boolean;
+        version: string;
+    }
+    /**
+     * A representation of the entire project managed by Lerna.
+     *
+     * Wherever the lerna.json file is located, that is the project root.
+     * All package globs are rooted from this location.
+     */
+    export class Project {
+        /**
+         *
+         * @param cwd {string} [cwd] defaults to process.cwd()
+         */
+        public static getPackges(cwd?: string): Promise<Package[]>;
+        /**
+         *
+         * @param cwd {string} [cwd] defaults to process.cwd()
+         */
+        public static getPackgesSync(cwd?: string): Package[];
+
+        public constructor(cwd: string);
+        public get version(): string;
+        public set version(val: string);
+        public get packageConfigs(): string[];
+        public get packageParentDirs(): string[];
+        public get manifest(): Package;
+        public get licensePath(): string;
+        public getPackages(): Promise<Package[]>;
+        public getPackagesSync(): Package[];
+        public getPackageLicensePaths(): string[];
+        public isIndependent(): boolean;
+        rootPath: string;
+        rootConfigLocation: string;
+        config: ProjectConfig;
+    }
+    export function getPackges(cwd?: string): Promise<Package[]>;
+    export function getPackgesSync(cwd?: string): Package[];
+}
---
+++ packages/lerna2codecov/src/update.spec.ts
@@ -0,0 +1,59 @@
+/*
+ * Package @donmahallem/lerna2codecov
+ * Source https://github.com/donmahallem/js-libs/tree/master/packages/lerna2codecov
+ */
+
+import { expect } from 'chai';
+import { promises as fsp } from 'fs';
+import 'mocha';
+import sinon from 'sinon';
+import { update } from './update';
+import * as updateConfig from './update-config';
+
+/* eslint-disable @typescript-eslint/no-unused-expressions, no-unused-expressions */
+describe('./update', (): void => {
+    let sandbox: sinon.SinonSandbox;
+    before('setup sandbox', (): void => {
+        sandbox = sinon.createSandbox();
+    });
+    afterEach('reset sandbox', (): void => {
+        sandbox.reset();
+    });
+    after('restore sandbox', (): void => {
+        sandbox.restore();
+    });
+    describe('update', (): void => {
+        let fspWriteStub: sinon.SinonStub;
+        let fspReadStub: sinon.SinonStub;
+        let updateConfigStub: sinon.SinonStub;
+        before('stup fs methods', (): void => {
+            fspWriteStub = sandbox.stub(fsp, 'writeFile');
+            fspReadStub = sandbox.stub(fsp, 'readFile');
+            updateConfigStub = sandbox.stub(updateConfig, 'updateConfig');
+        });
+        after((): void => {
+            updateConfigStub.restore();
+        });
+
+        it('should output the input if no packages are found in the lerna project', async (): Promise<void> => {
+            const codecovTestPath = './../../codecov.yml';
+            fspReadStub.resolves('');
+            updateConfigStub.resolves({ asdf: true });
+            await update('./', codecovTestPath);
+            expect(fspReadStub.callCount).to.equal(1);
+            expect(fspReadStub.getCall(0).args).to.deep.eq([codecovTestPath, 'utf-8']);
+            expect(fspWriteStub.callCount).to.equal(1);
+            expect(fspWriteStub.getCall(0).args).to.deep.eq([codecovTestPath, 'asdf: true\n', 'utf-8']);
+        });
+        it(`should create a codecov.yml if it doesn't exist`, async (): Promise<void> => {
+            const codecovTestPath = './../../codecov.yml';
+            fspReadStub.rejects(new Error('test error'));
+            updateConfigStub.resolves({ asdf: true });
+            await update('./', codecovTestPath);
+            expect(fspReadStub.callCount).to.equal(1);
+            expect(fspReadStub.getCall(0).args).to.deep.eq([codecovTestPath, 'utf-8']);
+            expect(fspWriteStub.callCount).to.equal(1);
+            expect(fspWriteStub.getCall(0).args).to.deep.eq([codecovTestPath, 'asdf: true\n', 'utf-8']);
+        });
+    });
+});
