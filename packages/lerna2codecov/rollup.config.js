fd1c7263b89649543707893e7a07d0868357b292
DonMahallem <donmahallem@users.noreply.github.com>
2022-01-03 14:09:18 +0100

    build(lerna2codecov): add cli build to rollup config

--- packages/lerna2codecov/rollup.config.js
+++ packages/lerna2codecov/rollup.config.js
@@ -1,4 +1,10 @@
 import defaultRollup from '@donmahallem/rollup-config';
 import pkg from './package.json';
 
-export default defaultRollup(pkg);
+const cliConfig = defaultRollup({ main: './dist/cli/cli.js' });//, { plugins: { commonjs: { esmExternals: true } } }
+cliConfig.input = './src/cli.ts';
+cliConfig.external = ["commander", "deepmerge", "yaml", "@lerna/project"];
+export default [
+    defaultRollup(pkg),
+    cliConfig,
+];
