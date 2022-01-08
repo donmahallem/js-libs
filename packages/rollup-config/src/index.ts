bf68e8ac44072b2422de039bc13cc3410d7151a9
DonMahallem <donmahallem@users.noreply.github.com>
2022-01-08 13:43:41 +0100

    chore(rollup-config): remove rollup-config package from monorepo

--- packages/rollup-config/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Package @donmahallem/rollup-config
- * Source https://donmahallem.github.io/js-libs/
- */
-
-export { baseConfig } from './base-config';
-export { baseConfig as default } from './base-config';
-export { baseDtsConfig } from './dts-config';
