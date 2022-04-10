2f7602210e838cbfeaf8fc10f508533f9b386954
donmahallem <donmahallem@users.noreply.github.com>
2022-04-10 14:18:49 +0200

    chore: remove unused projects

--- packages/remark-lerna-packages/src/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Package @donmahallem/remark-lerna-packages
- * Source https://donmahallem.github.io/js-libs/
- */
-
-import { remarkLernaPlugin } from './plugin';
-export { remarkLernaPlugin, IPluginOptions } from './plugin';
-export default remarkLernaPlugin;
