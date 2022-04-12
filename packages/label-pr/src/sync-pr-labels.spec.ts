3c4ee98f595bd1a378635d29a42ba84a704898e8
donmahallem <donmahallem@users.noreply.github.com>
2022-04-12 11:27:06 +0200

    style(label-pr): fix style issues

--- packages/label-pr/src/sync-pr-labels.spec.ts
+++ packages/label-pr/src/sync-pr-labels.spec.ts
@@ -1,8 +1,10 @@
-/*!
- * Source https://github.com/donmahallem/js-libs Package: label-pr
+/*
+ * Package @donmahallem/label-pr
+ * Source https://donmahallem.github.io/js-libs/
  */
 
 import * as labelgh from '@donmahallem/label-gh';
+import { Octokit } from '@octokit/core';
 import { expect } from 'chai';
 import 'mocha';
 import Sinon from 'sinon';
@@ -22,78 +24,97 @@ describe('syncLabels', (): void => {
     describe('syncLabels', (): void => {
         let getPullRequestLabelsStub: Sinon.SinonStub;
         let syncLabelsStub: Sinon.SinonStub;
+        let octoStub: Sinon.SinonStubbedInstance<Octokit>;
         before('setup octokit stub instance', (): void => {
             getPullRequestLabelsStub = sandbox.stub(labelgh, 'getPullRequestLabels');
             syncLabelsStub = sandbox.stub(labelgh, 'syncLabels');
+            octoStub = sandbox.createStubInstance(Octokit);
         });
         beforeEach('setup octokit stub instance', (): void => {
             syncLabelsStub.resolves('set label');
         });
         it('should ignore non prefixed labels and set prefixed', (): Promise<void> => {
             getPullRequestLabelsStub.resolves([{ name: 'label1' }]);
-            return syncPRLabels({} as any, {
-                owner: 'some_owner',
-                pull_number: 2,
-                repo: 'anyrepo',
-            }, ['test', 'label'])
-                .then((result: any): void => {
-                    expect(syncLabelsStub.callCount).to.equal(1, 'should be called');
-                    expect(syncLabelsStub.args).to.deep.eq([[{},
-                    {
-                        issue_number: 2,
-                        owner: 'some_owner',
-                        repo: 'anyrepo',
-                    }, [
-                        'pkg:test',
-                        'pkg:label',
-                        'label1',
-                    ], true]]);
-                    expect(result).to.equal('set label');
-                });
+            return syncPRLabels(
+                octoStub,
+                {
+                    owner: 'some_owner',
+                    pull_number: 2,
+                    repo: 'anyrepo',
+                },
+                ['test', 'label']
+            ).then((result: any): void => {
+                expect(syncLabelsStub.callCount).to.equal(1, 'should be called');
+                expect(syncLabelsStub.args).to.deep.eq([
+                    [
+                        {},
+                        {
+                            issue_number: 2,
+                            owner: 'some_owner',
+                            repo: 'anyrepo',
+                        },
+                        ['pkg:test', 'pkg:label', 'label1'],
+                        true,
+                    ],
+                ]);
+                expect(result).to.equal('set label');
+            });
         });
         it('should ignore non prefixed labels and set non default prefix', (): Promise<void> => {
             getPullRequestLabelsStub.resolves([{ name: 'label1' }, { name: 'asdf:any' }]);
-            return syncPRLabels({} as any, {
-                owner: 'some_owner',
-                pull_number: 2,
-                repo: 'anyrepo',
-            }, ['test', 'label'], 'asdf')
-                .then((result: any): void => {
-                    expect(syncLabelsStub.callCount).to.equal(1, 'should be called');
-                    expect(syncLabelsStub.args).to.deep.eq([[{},
-                    {
-                        issue_number: 2,
-                        owner: 'some_owner',
-                        repo: 'anyrepo',
-                    }, [
-                        'asdf:test',
-                        'asdf:label',
-                        'label1',
-                    ], true]]);
-                    expect(result).to.equal('set label');
-                });
+            return syncPRLabels(
+                octoStub,
+                {
+                    owner: 'some_owner',
+                    pull_number: 2,
+                    repo: 'anyrepo',
+                },
+                ['test', 'label'],
+                'asdf'
+            ).then((result: any): void => {
+                expect(syncLabelsStub.callCount).to.equal(1, 'should be called');
+                expect(syncLabelsStub.args).to.deep.eq([
+                    [
+                        {},
+                        {
+                            issue_number: 2,
+                            owner: 'some_owner',
+                            repo: 'anyrepo',
+                        },
+                        ['asdf:test', 'asdf:label', 'label1'],
+                        true,
+                    ],
+                ]);
+                expect(result).to.equal('set label');
+            });
         });
         it('should keep previous labels', (): Promise<void> => {
             getPullRequestLabelsStub.resolves([{ name: 'label1' }, { name: 'asdf:test' }]);
-            return syncPRLabels({} as any, {
-                owner: 'some_owner',
-                pull_number: 2,
-                repo: 'anyrepo',
-            }, ['test', 'label'], 'asdf')
-                .then((result: any): void => {
-                    expect(syncLabelsStub.callCount).to.equal(1, 'should be called');
-                    expect(syncLabelsStub.args).to.deep.eq([[{},
-                    {
-                        issue_number: 2,
-                        owner: 'some_owner',
-                        repo: 'anyrepo',
-                    }, [
-                        'asdf:label',
-                        'asdf:test',
-                        'label1',
-                    ], true]]);
-                    expect(result).to.equal('set label');
-                });
+            return syncPRLabels(
+                octoStub,
+                {
+                    owner: 'some_owner',
+                    pull_number: 2,
+                    repo: 'anyrepo',
+                },
+                ['test', 'label'],
+                'asdf'
+            ).then((result: any): void => {
+                expect(syncLabelsStub.callCount).to.equal(1, 'should be called');
+                expect(syncLabelsStub.args).to.deep.eq([
+                    [
+                        {},
+                        {
+                            issue_number: 2,
+                            owner: 'some_owner',
+                            repo: 'anyrepo',
+                        },
+                        ['asdf:label', 'asdf:test', 'label1'],
+                        true,
+                    ],
+                ]);
+                expect(result).to.equal('set label');
+            });
         });
     });
 });
