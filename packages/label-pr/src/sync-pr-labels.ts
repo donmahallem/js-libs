4c57d5d494ba3861a0912989765b91d5378146ab
ò_ó <donmahallem@users.noreply.github.com>
2022-04-12 14:50:06 +0200

    style(label-pr): replace tslint with eslint (#1420)
    * style(label-pr): replace tslint with eslint
    
    * style(label-pr): fix style issues
    
    * test(label-pr): fix test issues
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- packages/label-pr/src/sync-pr-labels.ts
+++ packages/label-pr/src/sync-pr-labels.ts
@@ -1,5 +1,6 @@
-/*!
- * Source https://github.com/donmahallem/js-libs Package: label-pr
+/*
+ * Package @donmahallem/label-pr
+ * Source https://donmahallem.github.io/js-libs/
  */
 
 import { calculateLabelDiff, getPullRequestLabels, syncLabels, GithubLabel, ILabelDiff } from '@donmahallem/label-gh';
@@ -19,31 +20,36 @@ export interface IOpts {
      */
     repo: string;
 }
-export const syncPRLabels = async (octokit: Octokit,
-    opts: IOpts,
-    packageLabel: string[],
-    prefix: string = 'pkg'): Promise<GithubLabel[]> => {
+export const syncPRLabels = async (octokit: Octokit, opts: IOpts, packageLabel: string[], prefix = 'pkg'): Promise<GithubLabel[]> => {
     const expectedLabels: string[] = packageLabel.map((baseLabel: string): string => {
         return `${prefix}:${baseLabel}`;
     });
     const prLabels: Partial<GithubLabel>[] = await getPullRequestLabels(octokit, opts);
-    const prLabelNames: string[] = prLabels.map((lab: GithubLabel): string | undefined => {
-        return lab.name;
-    }).filter((label: string | undefined): boolean => {
-        // tslint:disable-next-line:triple-equals
-        return label != undefined;
-    }) as string[];
+    const prLabelNames: string[] = prLabels
+        .map((lab: GithubLabel): string | undefined => {
+            return lab.name;
+        })
+        .filter((label: string | undefined): boolean => {
+            // tslint:disable-next-line:triple-equals
+            return label != undefined;
+        }) as string[];
     const diff: ILabelDiff = calculateLabelDiff(expectedLabels, prLabelNames);
     const finalLabels: string[] = [];
     finalLabels.push(...diff.add);
     finalLabels.push(...diff.unchanged);
-    finalLabels.push(...diff.remove
-        .filter((label: string): boolean => {
+    finalLabels.push(
+        ...diff.remove.filter((label: string): boolean => {
             return !label.startsWith(`${prefix}:`);
-        }));
-    return syncLabels(octokit, {
-        issue_number: opts.pull_number,
-        owner: opts.owner,
-        repo: opts.repo,
-    }, finalLabels, true);
+        })
+    );
+    return syncLabels(
+        octokit,
+        {
+            issue_number: opts.pull_number,
+            owner: opts.owner,
+            repo: opts.repo,
+        },
+        finalLabels,
+        true
+    );
 };
